9aef4c74a1d84aae1cf7ee0b66361a5473f1ea8f
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-03-24 15:27:45 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SignUp Controller', () => {
     // para objetos, arrays e funções, o toEqual compara o conteúdo, se usar o toBe, ele compara a referência
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided!', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    // para objetos, arrays e funções, o toEqual compara o conteúdo, se usar o toBe, ele compara a referência
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
