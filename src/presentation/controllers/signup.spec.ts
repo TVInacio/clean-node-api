94b7d87ab8fd67e802896eb75b5b56c9047b4e39
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-03-25 17:25:57 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,4 +31,18 @@ describe('SignUp Controller', () => {
     // para objetos, arrays e funções, o toEqual compara o conteúdo, se usar o toBe, ele compara a referência
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+  test('Should return 400 if no password is provided!', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    // para objetos, arrays e funções, o toEqual compara o conteúdo, se usar o toBe, ele compara a referência
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 })
