ab6a2a14bd0072e771f0ed29f049c1a3b983390b
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-03-25 10:50:41 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided!', () => {
@@ -13,7 +14,7 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     // para objetos, arrays e funções, o toEqual compara o conteúdo, se usar o toBe, ele compara a referência
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('Should return 400 if no email is provided!', () => {
@@ -28,6 +29,6 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     // para objetos, arrays e funções, o toEqual compara o conteúdo, se usar o toBe, ele compara a referência
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
