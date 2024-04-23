274a903cc2dd4407e2fcca7a8c07f155355c8ac3
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-04-23 15:11:15 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -13,6 +13,11 @@ describe('Account Mongo Repository', () => {
     return new AccountMongoRepository()
   }
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   it('Should return an account on success', async () => {
     const sut = makeSut()
     const account = await sut.add({
