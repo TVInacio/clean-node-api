38d9f27ea51f01254844d42ce0d70228283812a5
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-06-17 22:30:03 -0300

    chore: ignore main layer test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts', '!<rootDir>/src/**/*-protocols.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/main/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
