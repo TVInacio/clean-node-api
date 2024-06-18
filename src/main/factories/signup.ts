173ea08c535e4d6c4ff85272451811c5451963e9
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-06-17 21:41:55 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository) // Utilizamos o DbAddAccount(data layer) que é uma implementação de AddAccount(que é uma interface) que define o método add
+
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
