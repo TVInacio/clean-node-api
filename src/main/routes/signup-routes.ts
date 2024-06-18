c627c6528611e7148e7e2725d7a858e21229b20f
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-06-17 22:20:30 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/signup'
 
 export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  // isto vai retornar uma instância de controlador e ele vai adaptar o controlador para uma assinatura que o express entenda
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
