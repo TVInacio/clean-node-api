a4e6a7e340b8f606f27867898a171a107625154e
Tiago Veronesi <veronesi.tiago@gmail.com>
2024-06-30 21:33:25 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,5 +1,4 @@
 import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
@@ -11,11 +10,11 @@ import { makeSignUpValidation } from './signup-validation'
 export const makeSignUpController = (): Controller => {
   const salt = 12
 
-  const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository) // Utilizamos o DbAddAccount(data layer) que é uma implementação de AddAccount(que é uma interface) que define o método add
   const logMongoRepository = new LogMongoRepository()
-  const signUp = new SignUpController(emailValidatorAdapter, dbAddAccount, makeSignUpValidation())
+
+  const signUp = new SignUpController(dbAddAccount, makeSignUpValidation())
   return new LogControllerDecorator(signUp, logMongoRepository)
 }
